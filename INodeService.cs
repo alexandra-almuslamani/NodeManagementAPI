f64432f34c857da7225bedd544bccb9c5a551d1e
Alexandra Almuslamani <164527663+alexandra-almuslamani@users.noreply.github.com>
2026-03-05 03:10:08 +0300

    Refactor namespaces and using directives in INodeService

---
+++ INodeService.cs
@@ -0,0 +1,28 @@
+﻿using NodeManagementAPI.Core.Dtos;
+using NodeManagementAPI.Core.Dtos.NodeDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeManagementAPI.Services.Contract.INodeContracts
+{
+    public interface INodeService
+    {
+        public Task<ResultDto> AddNode(AddNodeDTO addNodeDTO);
+        public Task<ResultDto> UpdateNode(UpdateNodeDTO updateNode);
+        public Task<ResultDto> DeleteNode(int Id);
+        public Task<List<NodeResponseDTO>> GetAllNodes();
+        public Task<NodeResponseDTO> GetNodeById(int Id);
+        public Task<UpdateNodeDTO> GetNodeByName(string Name);
+        public Task<List<NodeResponseDTO>> GetChildrenByParentId(int parentId);
+        public Task<List<NodeResponseDTO>> GetAllDescendantsByParentId(int parentId);
+        // add root node for tenant to help get tenant children
+        public Task<NodeIdResponseDTO> AddRootNode(AddNodeDTO nodeDTO);
+        public Task<ResultDto> UpdateRootStatusNode(UpdateNodeDTO updateNode);
+        //get the id of last added node 
+        public Task<int?> GetLastAddedNodeId();
+        public Task<double> CalculateNodePercentage(int NodeId);
+    }
+}
