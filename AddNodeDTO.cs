d18f00a561e906500b62eb8c56ae44d9e5c3ba56
Alexandra Almuslamani <164527663+alexandra-almuslamani@users.noreply.github.com>
2026-03-05 03:32:55 +0300

    Rename namespace to NodeManagementAPI

---
+++ AddNodeDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeManagementAPI.Core.Dtos.NodeDTO
+{
+    public class AddNodeDTO
+    {
+        public string NameAr { get; set; }
+        public string NameEn { get; set; }
+        public string DescriptionAr { get; set; }
+        public string DescriptionEn { get; set; }
+        public int? ParentId { get; set; }
+        public int NodeTypeId { get; set; }
+
+    }
+}
