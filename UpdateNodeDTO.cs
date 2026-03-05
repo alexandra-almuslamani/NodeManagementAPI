915341b217037f962cc51e213519974cab934f84
Alexandra Almuslamani <164527663+alexandra-almuslamani@users.noreply.github.com>
2026-03-05 03:38:08 +0300

    Change namespace for UpdateNodeDTO class

---
+++ UpdateNodeDTO.cs
@@ -0,0 +1,21 @@
+using NodeManagementAPI.Core.Dtos.PagingDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeManagementAPI.Core.Dtos.NodeDTO
+{
+    public class UpdateNodeDTO : IPagingParent
+    {
+        public int Id { get; set; }
+        public string NameAr { get; set; }
+        public string NameEn { get; set; }
+        public string DescriptionAr { get; set; }
+        public string DescriptionEn { get; set; }
+        public int? ParentId { get; set; }
+        public int NodeTypeId { get; set; }
+        public int? StatusId { get; set; }
+    }
+}
