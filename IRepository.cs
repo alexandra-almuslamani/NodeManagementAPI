87ae58e55a1868147d411ce9a7fa35b4cb74081f
Alexandra Almuslamani <164527663+alexandra-almuslamani@users.noreply.github.com>
2026-03-05 03:47:37 +0300

    Rename namespace to NodeManagementAPI

---
+++ IRepository.cs
@@ -0,0 +1,19 @@
+﻿using NodeManagementAPI.Core.Dtos;
+using NodeManagementAPI.Core.Entities.OrgManagementEntities;
+using System.Linq.Expressions;
+
+
+namespace NodeManagementAPI.Core.IRepositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T?>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task<ResultDto> AddAsync(T entity);
+        Task<ResultDto> UpdateAsync(T entity);
+        Task<ResultDto> RemoveAsync(T entity);
+        Task<ResultDto> RemoveAsync(int id);
+        Task<IEnumerable<T?>> GetByConditionAsync(Expression<Func<T, bool>> condition);
+        Task<IEnumerable<Node>> GetChildrenAsync(int parentId);
+    }
+}
