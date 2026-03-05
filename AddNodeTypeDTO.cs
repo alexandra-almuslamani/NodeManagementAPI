c175369fe1cc97cb6f22cfc1c1a832839844e89b
Alexandra Almuslamani <164527663+alexandra-almuslamani@users.noreply.github.com>
2026-03-05 03:38:44 +0300

    Rename namespace for AddNodeTypeDTO class

---
+++ AddNodeTypeDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeManagementAPI.Core.Dtos.NodeTypeDTO
+{
+    public class AddNodeTypeDTO
+    {
+        public string? TypeOfNode { get; set; }
+    }
+}
