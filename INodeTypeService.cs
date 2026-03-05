fbc712b5724d19aac534daf9b85aaec71eddba4d
Alexandra Almuslamani <164527663+alexandra-almuslamani@users.noreply.github.com>
2026-03-05 03:11:13 +0300

    Refactor namespaces and using directives in INodeTypeService

---
+++ INodeTypeService.cs
@@ -0,0 +1,19 @@
+﻿using NodeManagementAPI.Core.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NodeManagementAPI.Core.Dtos.NodeTypeDTO;
+
+namespace NodeManagementAPI.Services.Contract.INodeTypeContracts
+{
+    public interface INodeTypeService
+    {
+        public Task<ResultDto> AddNodeType(AddNodeTypeDTO addNodeTypeDTO);
+        public Task<ResultDto> UpdateNodeType(NodeTypeResponseDTO updateNodeType);
+        public Task<ResultDto> DeleteNodeType(int Id);
+        public Task<List<NodeTypeResponseDTO>> GetAllNodeTypes();
+        public Task<NodeTypeResponseDTO> GetNodeTypeById(int Id);
+    }
+}
