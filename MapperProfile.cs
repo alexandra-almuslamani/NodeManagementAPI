1c656835b90e04339b3b88be1a7747c15a2a6668
Alexandra Almuslamani <164527663+alexandra-almuslamani@users.noreply.github.com>
2026-03-05 03:31:28 +0300

    Create MapperProfile.cs

---
+++ MapperProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using NodeManagementAPI.Core.Dtos;
+using NodeManagementAPI.Core.Entities;
+using NodeManagementAPI.Core.Dtos.NodeDTO;
+using NodeManagementAPI.Core.Dtos.NodeTypeDTO;
+
+namespace NodeManagementAPI.Infrastructure.MaperProfile
+{
+	public class MapperProfile : Profile
+	{
+		public MapperProfile()
+		{
+       CreateMap<AddNodeDTO, Node>();
+       CreateMap<Node, NodeResponseDTO>();
+       CreateMap<NodeResponseDTO, Node>();
+       CreateMap<Node, NodeIdResponseDTO>();
+       CreateMap<Node, UpdateNodeDTO>();
+       CreateMap<NodeResponseDTO, UpdateNodeDTO>();
+
+       CreateMap<AddNodeTypeDTO, NodeType>();
+       CreateMap<NodeType, NodeTypeResponseDTO>();
+    }
+  }
+}
+      
